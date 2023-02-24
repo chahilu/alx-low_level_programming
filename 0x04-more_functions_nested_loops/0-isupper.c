b70aa2b320a2afb75a0dd399cb618489bfa20989
chahilu <chahilusheila@gmail.com>
2023-02-24 10:40:28 -0800

    ispuer

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * _suppoer - checks for uppercase letters
+* _suppoer - checks for uppercase letters
 *@c : The character being checked
 *
 *Return: 1 if character is upper case 0 other wise
 */
 int _supper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if ((c >= 'A') && (c <= 'Z'))
 	return (1);
+
 	else
-	return (0);
+		return (0);
 }
